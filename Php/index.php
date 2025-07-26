e17925b9ca74f810f63686fcc859989cae1c964b
Brunis1108 <bruna.a.ba76@gmail.com>
2025-07-26 13:44:14 -0300

    Subindo primeira versão

---
+++ Php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Minha Página PHP</title>
+    <link rel="stylesheet" href="style.css"> <!-- CSS externo -->
+</head>
+<body>
+    <h1>Olá, mundo em PHP!</h1>
+    
+    <?php
+        echo "<p>Esse parágrafo foi impresso com PHP.</p>";
+    ?>
+</body>
+</html>
